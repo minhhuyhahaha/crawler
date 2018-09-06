41bbff7c005bba956cf1ddefe04fb98c288fc52b
Nguyen Van Thanh <songtinhcam03@gmail.com>
2018-09-06 10:28:36 +0700

    source

--- my_module/database.js
+++ my_module/database.js
@@ -9,6 +9,11 @@ var con = mysql.createConnection({
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
+    let sql = "CREATE TABLE IF NOT EXISTS `advertising` (`id` int(11) NOT NULL PRIMARY KEY,`url` text,`title` text,`content` text,`image` text,`info_estate` text,`info_project` text,`name` text,`address` text,`email` text,`phone` text,`area` text,`price` text,`area_size` text) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+    con.query(sql, (err, results, fields) => {
+        if (err) throw err;
+        console.log(results);
+    });
 });
 
 function insert(data){
